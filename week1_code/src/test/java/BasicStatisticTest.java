3b60ed8d8483b2659408d382287b0ea226834e52
Ali <tkfreak01@gmail.com>
2018-11-18 15:03:58 -0800

    tested sum function

---
+++ week1_code/src/test/java/BasicStatisticTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BasicStatisticTest {
+
+    @Test
+    public void addDoubleToData() {
+    }
+
+    @Test
+    public void clearData() {
+
+    }
+
+    @Test
+    public void numberOfDataItems() {
+    }
+
+    @Test
+    public void sum() {
+        BasicStatistic Bs=new BasicStatistic();
+        double result=Bs.sum();
+        assertEquals(0.0,result,0);
+    }
+
+    @Test
+    public void highestValue() {
+    }
+
+    @Test
+    public void getMean() {
+    }
+
+    @Test
+    public void getMedian() {
+    }
+
+    @Test
+    public void getStandardDeviation() {
+    }
+}
